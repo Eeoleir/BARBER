86974f29aeed761aceae08fc9664702cbc1b7142
Kent <asilo.kentbryanv19@gmail.com>
2023-05-01 19:22:40 +0800

    SERVICES

---
+++ JS/services.js
@@ -0,0 +1,25 @@
+
+// WESTERN
+document.getElementById("open-btn").addEventListener("click", function () {
+  document.body.classList.add("active-popup1");
+});
+
+document.getElementById("close-btn").addEventListener("click", function () {
+  document.body.classList.remove("active-popup1");
+});
+// KOREAN
+document.getElementById("open-btn2").addEventListener("click", function () {
+  document.body.classList.add("active-popup2");
+});
+
+document.getElementById("close-btn2").addEventListener("click", function () {
+  document.body.classList.remove("active-popup2");
+});
+// PINOY
+document.getElementById("open-btn3").addEventListener("click", function () {
+  document.body.classList.add("active-popup3");
+});
+
+document.getElementById("close-btn3").addEventListener("click", function () {
+  document.body.classList.remove("active-popup3");
+});
